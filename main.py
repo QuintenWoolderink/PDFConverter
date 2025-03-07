072780068e6c07885dbf3ea3e133834760c27396
Quinten Woolderink <quintencoderen@gmail.com>
2025-03-07 21:50:42 +0100

    .docx file compattable
    made .docx file compattable

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from converter import md_to_pdf
 
 def main():
     parser = argparse.ArgumentParser(description="Converts Markdown or Text files to PDF")
-    parser.add_argument("input", help="Input file (.md or .txt)")
+    parser.add_argument("input", help="Input file (.md or .txt or .docx)")
     parser.add_argument("--output", help="Output PDF file", default="output.pdf")
     parser.add_argument("--font", help="Custom font for the PDF", default="Helvetica")
     parser.add_argument("--page-size", help="Page size for the PDF", default="letter")
